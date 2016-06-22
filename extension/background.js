838a078175f959f244128643a4916cd5bf56e74b
Jeff Hoye <jeffhoye@gmail.com>
2016-06-22 08:44:03 -0400

    set the reportURL on the plugin

--- extension/background.js
+++ extension/background.js
@@ -1,4 +1,4 @@
-var reportURL = "http://localhost:7677/reportPrice/";
+var reportURL = "https://price.volunteerscience.com/reportPrice/";
 var supportedSites = [{"site": "https://www.google.com/flights/", "script":"google-flights"},
                       {"site": "https://www.amazon.com/s/", "script":"amazon"}];
 
